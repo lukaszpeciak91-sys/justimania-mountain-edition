0a08009c0781787f62ad4bb1967cf22aa92bbc34
lukaszpeciak91-sys <lukaszpeciak91@gmail.com>
2026-08-29 22:44:36 +0200

    Match BACK button size to START

--- src/ui/menuLayout.js
+++ src/ui/menuLayout.js
@@ -1,11 +1,16 @@
+const MENU_BUTTON_SIZE = Object.freeze({
+  width: 190,
+  height: 68,
+});
+
 export const MENU_LAYOUT = Object.freeze({
   startYRatio: 0.5,
   startEntranceOffset: 8,
-  startWidth: 190,
-  startHeight: 68,
+  startWidth: MENU_BUTTON_SIZE.width,
+  startHeight: MENU_BUTTON_SIZE.height,
   backGap: 78,
-  backWidth: 140,
-  backHeight: 54,
+  backWidth: MENU_BUTTON_SIZE.width,
+  backHeight: MENU_BUTTON_SIZE.height,
 });
 
 export function menuControlLayout(viewportWidth, viewportHeight) {
